3ad0837798c5a26b12d91837a760664667f4a406
yugld <yulliagladysheva@gmail.com>
2025-07-31 11:10:14 +0300

    base eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,36 @@
-import { defineConfig } from "eslint-define-config";
+import js from "@eslint/js";
+import globals from "globals";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
-const eslintRecommended = {
-    rules: {},
+const prettierOptions = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
 };
 
-const prettierConfig = {
-    rules: {},
-};
-
-export default defineConfig([
+export default [
     {
+        ignores: ["dist"],
+        files: ["**/*.{js,mjs,cjs}"],
         languageOptions: {
-            ecmaVersion: 2021,
-            sourceType: "module",
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
-        plugins: {},
+        plugins: {
+            prettier: eslintPluginPrettier,
+        },
         rules: {},
     },
-    eslintRecommended,
-    prettierConfig,
-]);
+    {
+        ...js.configs.recommended,
+    },
+    {
+        plugins: {
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            "prettier/prettier": ["error", prettierOptions],
+        },
+    },
+];
